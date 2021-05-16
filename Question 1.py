06bed3b19cea92fe30965fa9905de3e54012383e
daeronhing <31173368+daeronhing@users.noreply.github.com>
2021-05-16 12:26:32 +0800

    Question 1 & 2

--- Question 1.py
+++ Question 1.py
@@ -1,6 +1,10 @@
 import numpy as np
 import openravepy as orpy
+from openravepy import *
 import time
+import matplotlib.pyplot as plt
+import matplotlib.backends.backend_pdf
+
 env = orpy.Environment()
 env.SetViewer('qtcoin')
 env.Load('osr_openrave/worlds/pick_and_place.env.xml')
@@ -51,16 +55,20 @@ for i in range(n_layer):
 robot = env.GetRobots()[0]
 manipulator = robot.SetActiveManipulator('gripper')
 robot.SetActiveDOFs(manipulator.GetArmIndices())
+manipprob = interfaces.BaseManipulation(robot)
+
 np.set_printoptions(precision=6, suppress=True)
 
 ikmodel = orpy.databases.inversekinematics.InverseKinematicsModel(robot, iktype=orpy.IkParameterization.Type.Transform6D)
 if not ikmodel.load():
   ikmodel.autogenerate()
 
-# bi-RRT setup #
-planner = orpy.RaveCreatePlanner(env, 'birrt')
-params = orpy.Planner.PlannerParameters()
-params.SetPostProcessing('ParabolicSmoother', '<_nmaxiterations>40</_nmaxiterations>')
+lmodel = databases.linkstatistics.LinkStatisticsModel(robot)
+if not lmodel.load():
+  lmodel.autogenerate()
+
+lmodel.setRobotResolutions(0.01)
+lmodel.setRobotWeights()
 
 # Functions declaration #
 def get_Tgrasp(input_box, side = 1):
@@ -88,9 +96,12 @@ def get_Tgoal(destination, n, side, tilt_tries = 0, slide_tries = 0):
   
   return T
 
-def tilt(T):
+def tilt(T, direction = 1):
   t = np.eye(4)
-  angle = 5 * np.pi/180
+  if direction == 1:
+    angle = 5 * np.pi / 180
+  else:
+    angle = -5 * np.pi / 180
   t[1,1] = np.cos(angle)
   t[1,2] = -np.sin(angle)
   t[2,1] = np.sin(angle)
@@ -103,36 +114,33 @@ def slide(T):
   return(np.dot(T,t))
 
 def to_pick(qgrasp):
-  params.SetRobotActiveJoints(robot)
-  params.SetGoalConfig(qgrasp)
-  planner.InitPlan(robot, params)
-  traj = orpy.RaveCreateTrajectory(env, '')
-  planner.PlanPath(traj)
-  controller = robot.GetController()
-  controller.SetPath(traj)
+  traj = manipprob.MoveManipulator(goal = qgrasp, execute = False, outputtrajobj = True)
+  return(traj)
 
 def to_put(qgoal):
-  params.SetRobotActiveJoints(robot)
-  params.SetGoalConfig(qgoal)
-  planner.InitPlan(robot, params)
-  traj = orpy.RaveCreateTrajectory(env, '')
-  planner.PlanPath(traj)
-  controller = robot.GetController()
-  controller.SetPath(traj)
+  traj = manipprob.MoveManipulator(goal = qgoal, execute = False, outputtrajobj = True)
+  return(traj)
 
 '''------------------------------------Preparation Done---------------------------------------------------------------------------------------------'''
 
 # Main code #
 n = 0
-theta = 5 * np.pi/180
 tilt_tries = 0
 slide_tries = 0
 grab_side = 1
 successful = 0
 cmi = 0
 
+all_time_tick = []
+all_tilt_tries = []
+all_put_traj = []
+all_box_id = []
+
+start_time = time.time()
+
 for i in range(-1, -41, -1):
   selected_box = boxes[i]
+  box_id = i + 40
   Tgrasp = get_Tgrasp(selected_box)
   qgrasp = manipulator.FindIKSolutions(Tgrasp, orpy.IkFilterOptions.CheckEnvCollisions)
 
@@ -183,7 +191,7 @@ for i in range(-1, -41, -1):
   if(not len(qgrasp)):
     cmi += 1
     env.Remove(selected_box)
-    print('Box with index', i+40, 'cannot be picked up and is removed.')
+    print('Box with index', box_id, 'cannot be picked up and is removed.')
     print('successful = ', successful)
     print('cmi = ', cmi)
     tilt_tries = 0
@@ -202,20 +210,31 @@ for i in range(-1, -41, -1):
   if(not len(qgoal)):
     cmi += 1
     env.Remove(selected_box)
-    print('Box with index', i+40, 'cannot be placed down and is removed.')
+    print('Box with index', box_id, 'cannot be placed down and is removed.')
     print('successful = ', successful)
     print('cmi = ', cmi)
     continue
 
-  to_pick(qgrasp[0])
+  pick_traj = to_pick(qgrasp[0])
+  robot.GetController().SetPath(pick_traj)
   robot.WaitForController(0)
   robot.Grab(selected_box)
-  time.sleep(0)
   
-  to_put(qgoal[0])
+  put_traj = to_put(qgoal[0])
+  robot.GetController().SetPath(put_traj)
   robot.WaitForController(0)
   robot.Release(selected_box)
-  time.sleep(0)
+
+  # Question 1 ends here #
+
+  # Question 2
+
+  time_tick = np.arange(0, put_traj.GetDuration(), 0.01)
+  
+  all_time_tick.append(time_tick)
+  all_tilt_tries.append(tilt_tries)
+  all_put_traj.append(put_traj)
+  all_box_id.append(box_id)
 
   successful += 1
   
@@ -223,8 +242,49 @@ for i in range(-1, -41, -1):
   slide_tries = 0
   grab_side = 1
 
+end_time = time.time()
+
+pdf = matplotlib.backends.backend_pdf.PdfPages('for_testing.pdf')
+
+for i in range(len(all_time_tick)):
+  time_tick = all_time_tick[i]
+  tilt_tries = all_tilt_tries[i]
+  put_traj = all_put_traj[i]
+  box_id = all_box_id[i]
+  spec = put_traj.GetConfigurationSpecification()
+  q_tick = np.zeros((len(time_tick), robot.GetActiveDOF()))
+  print('box id = ', box_id)
+
+  for tick in range(len(time_tick)):
+    trajdata = put_traj.Sample(time_tick[tick])
+    q_tick[tick,:] = spec.ExtractJointValues(trajdata, robot, manipulator.GetArmIndices(), 0)
+
+    angles = np.zeros(len(time_tick))
+    with robot:
+      for tick in range(len(time_tick)):
+        robot.SetActiveDOFValues(q_tick[tick,:])
+        T = manipulator.GetEndEffectorTransform()
+        if(tilt_tries != 0):
+          for tries in range(tilt_tries):
+            T = tilt(T,-1)
+            if abs(T[2, 2]) > 1:
+                T[2, 2] = T[2, 2] / abs(T[2, 2])
+        angles[tick] = (180 - np.arccos(T[2, 2]) * 180 / np.pi)
+  print('box id', box_id, 'angles = ', angles)
+  fig = plt.figure(i)
+  ax = fig.gca()
+  plt.plot(time_tick, angles, 'r', linewidth = 2, label='box %d'%box_id)
+  ax.set_xlabel('Time [s]')
+  ax.set_ylabel('Tilt angles [deg]')
+  plt.grid()
+  plt.legend()
+  pdf.savefig(fig)
+
+pdf.close()
+
 print('successful = ', successful)
 print('cmi = ', cmi)
 print('It is done!')
+print('Time Taken = ', (end_time - start_time)/60)
 
-raw_input('Press anykey to quit')
+raw_input('Press enter to quit')
